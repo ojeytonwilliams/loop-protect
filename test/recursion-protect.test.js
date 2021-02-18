e92026c1f9192250f8ca65a8aa6f528e4ee832db
Shaun Hamilton <2397860h@student.gla.ac.uk>
2021-02-18 13:03:09 +0000

    increase timeout for React tests

--- test/recursion-protect.test.js
+++ test/recursion-protect.test.js
@@ -4,7 +4,7 @@ const Enzyme = require("enzyme");
 const Adapter = require("enzyme-adapter-react-16");
 Enzyme.configure({ adapter: new Adapter() });
 const Babel = require("@babel/standalone");
-Babel.registerPlugin("loopProtection", require("../lib")(100));
+Babel.registerPlugin("loopProtection", require("../lib")(300));
 Babel.registerPreset(
   "React",
   require("../node_modules/@babel/preset-react/lib")
@@ -56,7 +56,7 @@ const code = {
       render() {
         return (
           <div>
-            <button onClick = {this.handleClick}>Click Me</button>
+            <button onClick={this.handleClick}>Click Me</button>
             <h1>{this.state.name}</h1>
           </div>
         );
@@ -80,8 +80,8 @@ const loopProtect = (code) =>
   }).code; // eslint-disable-line no-new-func
 const run = (code) => eval(`${code}`); // eslint-disable-line no-eval
 
-describe("recursion", function () {
-  beforeEach(function () {
+describe("recursion", () => {
+  beforeEach(() => {
     spy = sinon.spy(run);
   });
 
@@ -105,48 +105,48 @@ describe("recursion", function () {
     expect(compiled).not.toBe(code);
   });
 
-  it("should rewrite class components", function () {
-    var c = code.simpleclass;
-    var compiled = loopProtect(c);
+  it("should rewrite class components", () => {
+    const c = code.simpleclass;
+    const compiled = loopProtect(c);
     expect(compiled).not.toBe(c);
-    var result = run(compiled);
+    const result = run(compiled);
     expect(result).toBe(1);
   });
 
-  it("should protect infinite class components", function () {
-    var c = code.infiniteclass;
-    var compiled = loopProtect(c);
+  it("should protect infinite class components", () => {
+    const c = code.infiniteclass;
+    const compiled = loopProtect(c);
 
     expect(run(compiled)).toBe(true);
     expect(compiled).not.toBe(c);
   });
 
-  it("should protect infinite functional component", function () {
-    var c = code.infinitefuncclass;
-    var compiled = loopProtect(c);
+  it("should protect infinite functional component", () => {
+    const c = code.infinitefuncclass;
+    const compiled = loopProtect(c);
     // assert(compiled !== c);
     // assert(spy(compiled) === 0);
     expect(run(compiled).props.children[0]).toBe("Some content with");
     expect(compiled).not.toBe(c);
   });
 
-  it("should allow functional components to run", function () {
-    var c = code.simplefunctionalclass;
-    var compiled = loopProtect(c);
-    var r = run(compiled);
+  it("should allow functional components to run", () => {
+    const c = code.simplefunctionalclass;
+    const compiled = loopProtect(c);
+    const r = run(compiled);
     expect(compiled).not.toBe(c);
     expect(r).toStrictEqual(<div>Some Value</div>);
   });
 
-  it('should handle non-keyword "class"', function () {
-    var c = code.notclass;
-    var compiled = loopProtect(c);
-    var result = run(compiled);
+  it('should handle non-keyword "class"', () => {
+    const c = code.notclass;
+    const compiled = loopProtect(c);
+    const result = run(compiled);
     expect(compiled).toBe(c);
     expect(result).toBe(true);
   });
 
-  it("should not break with .replace()", function () {
+  it("should not break with .replace()", () => {
     const removeAssignments = (str) =>
       str
         .replace(/myNoun\s*=\s*["']dog["']/g, "")
@@ -154,10 +154,10 @@ describe("recursion", function () {
         .replace(/myVerb\s*=\s*["']ran["']/g, "")
         .replace(/myAdverb\s*=\s*["']quickly["']/g, "");
 
-    var c = code.withreplace;
-    var compiled = loopProtect(c);
-    var newCode = removeAssignments(compiled);
-    var result = run(compiled);
+    const c = code.withreplace;
+    const compiled = loopProtect(c);
+    const newCode = removeAssignments(compiled);
+    const result = run(compiled);
 
     expect(
       !/dog/.test(newCode) &&
@@ -173,19 +173,19 @@ describe("recursion", function () {
     ).toBe(true);
   });
 
-  it("should not break on stacked arrow functions", function () {
-    var c = code.stackedarrowfunctions;
-    var compiled = loopProtect(c);
-    var result = run(compiled);
+  it("should not break on stacked arrow functions", () => {
+    const c = code.stackedarrowfunctions;
+    const compiled = loopProtect(c);
+    const result = run(compiled);
     expect(result(2)(4)(6)).toBe(12);
   });
 
-  it("should allow Enzyme.mount state handling", async function () {
+  it("should allow Enzyme.mount state handling", async () => {
+    // NOTE: Can fail if loopProtect timeout is too low
     const test = async (MyComponent) => {
       const waitForIt = (fn) =>
         new Promise((resolve, reject) => setTimeout(() => resolve(fn()), 250));
       const mockedComponent = Enzyme.mount(React.createElement(MyComponent));
-      console.log("mock: ", mockedComponent);
       const first = () => {
         mockedComponent.setState({ name: "Before" });
         return waitForIt(() => mockedComponent.state("name"));
@@ -199,11 +199,9 @@ describe("recursion", function () {
       expect(firstValue).toStrictEqual("Before");
       expect(secondValue).toStrictEqual("React Rocks!");
     };
-    var c = code.mockclass;
-    var compiled = loopProtect(c);
-    console.log("comp: ", compiled);
-    var result = run(compiled);
-    console.log("res: ", result);
+    const c = code.mockclass;
+    const compiled = loopProtect(c);
+    const result = run(compiled);
     await test(result);
   });
 });
